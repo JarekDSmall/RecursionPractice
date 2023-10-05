377796774c3b448d6bc2fcd8375de906e5f402c1
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-10-05 17:21:36 -0400

    Implement recursive Boggle solution in JavaScript

--- boggle/boggle.js
+++ boggle/boggle.js
@@ -1,42 +1,5 @@
-/** Boggle word check.
-
-Given a 5x5 boggle board, see if you can find a given word in it.
-
-In Boggle, you can start with any letter, then move in any NEWS direction.
-You can continue to change directions, but you cannot use the exact same
-tile twice.
-
-So, for example::
-
-    N C A N E
-    O U I O P
-    Z Q Z O N
-    F A D P L
-    E D E A Z
-
-In this grid, you could find `NOON* (start at the `N` in the top
-row, head south, and turn east in the third row). You cannot find
-the word `CANON` --- while you can find `CANO` by starting at the
-top-left `C`, you can 't re-use the exact same `N` tile on the
-front row, and there's no other `N` you can reach.
-
-*/
-
 function makeBoard(boardString) {
-  /** Make a board from a string.
-
-    For example::
-
-        board = makeBoard(`N C A N E
-                           O U I O P
-                           Z Q Z O N
-                           F A D P L
-                           E D E A Z`);
-
-        board.length   // 5
-        board[0]       // ['N', 'C', 'A', 'N', 'E']
-    */
-
+  /** Make a board from a string. */
   const letters = boardString.split(/\s+/);
 
   const board = [
@@ -52,12 +15,43 @@ function makeBoard(boardString) {
 
 function find(board, word) {
   /** Can word be found in board? */
-  // TODO
+
+  const rows = board.length;
+  const cols = board[0].length;
+
+  function isValid(x, y, visited) {
+    return x >= 0 && y >= 0 && x < rows && y < cols && !visited[`${x},${y}`];
+  }
+
+  function search(x, y, index, visited) {
+    if (index === word.length) return true;
+    if (!isValid(x, y, visited) || board[x][y] !== word[index]) return false;
+
+    visited[`${x},${y}`] = true;
+
+    // NEWS directions
+    const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+    for (let [dx, dy] of directions) {
+      if (search(x + dx, y + dy, index + 1, { ...visited })) {
+        return true;
+      }
+    }
+
+    visited[`${x},${y}`] = false;
+    return false;
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (search(i, j, 0, {})) return true;
+    }
+  }
+
+  return false;
 }
 
 // EXAMPLE TEST
-
-// For example::
 
 const board = makeBoard(`N C A N E
                          O U I O P
@@ -65,31 +59,25 @@ const board = makeBoard(`N C A N E
                          F A D P L
                          E D E A Z`);
 
-// `NOON` should be found (0, 3) -> (1, 3) -> (2, 3) -> (2, 4)::
-
+// `NOON` should be found (0, 3) -> (1, 3) -> (2, 3) -> (2, 4):
 console.log(find(board, "NOON"), true);
 
-// `NOPE` should be found (0, 3) -> (1, 3) -> (1, 4) -> (0, 4)::
-
+// `NOPE` should be found (0, 3) -> (1, 3) -> (1, 4) -> (0, 4):
 console.log(find(board, "NOPE"), true);
 
 // `CANON` can't be found (`CANO` starts at (0, 1) but can't find
-// the last `N` and can't re-use the N)::
-
+// the last `N` and can't re-use the N):
 console.log(find(board, "CANON"), false);
 
 // You cannot travel diagonally in one move, which would be required
-// to find `QUINE`::
-
+// to find `QUINE`:
 console.log(find(board, "QUINE"), false);
 
-// We can recover if we start going down a false path (start 3, 0)::
-
+// We can recover if we start going down a false path (start 3, 0):
 console.log(find(board, "FADED"), true);
 
 // An extra tricky case --- it needs to find the `N` toward the top right,
-// and then go down, left, up, up, right to find all four `O`s and the `S`::
-
+// and then go down, left, up, up, right to find all four `O`s and the `S`:
 const board2 = makeBoard(`E D O S Z
                           N S O N R
                           O U O O P
